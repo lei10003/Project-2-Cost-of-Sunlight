3088721e9e52c6b705b947f02c6c36bd90423d2f
lei10003 <ligea.alex@gmail.com>
2023-07-19 18:44:48 -0400

    Updating html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
@@ -9,14 +11,7 @@ module.exports = {
 module.exports = {
 
   plugins: [
-      require('flowbite/plugin')
-  ]
-
-}
-module.exports = {
-
-  content: [
-      "./node_modules/flowbite/**/*.js"
+    require('flowbite/plugin')
   ]
 
 }
